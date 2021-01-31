c9d3d5a74480fb2c37fb45a8b7c2d5b3360c4bbd
Alexander <freemanspeed0738@gmail.com>
2021-01-31 11:09:28 +0200

    App skeleton

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="Header">
+            <img src="https://www.mintformations.co.uk/blog/wp-content/uploads/2020/05/shutterstock_583717939.jpg" />
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTT-_mIfmXfxZydIr5sSyiVu6p_LMiG6L8ZDw&usqp=CAU" />
+            </div>
+            <div>
+                <img src="https://w3schoolsrus.github.io/howto/img_avatar.png" />
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My Post
+              <div>
+                    New post
+                </div>
+                <div>
+                    Post #1
+                 </div>
+                <div>
+                    Post #2
+                </div>
+            </div>
+        </div>
+
+    );
+}
+
+export default Profile;
