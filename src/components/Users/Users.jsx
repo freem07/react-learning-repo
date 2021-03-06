d83d2986393eb03fc7bf0cf6fdb92f0f495c3eec
Alexander <freemanspeed0738@gmail.com>
2021-03-06 21:41:45 +0200

    work with pagination

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -7,14 +7,14 @@ import userPhoto from './../../assets/images/def-ava.png';
 class Users extends React.Component {
   
     componentDidMount(){
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
                 this.props.setUsers(response.data.items);
                 this.props.setTotalUsersCount(response.data.totalCount)
             })
     }
     onPageChanged = (pageNumber) =>{
         this.props.setCurrentPage(pageNumber)
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page${pageNumber}&count=${this.props.pageSize}`).then(response => {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
             this.props.setUsers(response.data.items)
         })
     }
