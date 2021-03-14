fd3715af8ee512065f73ca09f0be92fd228ce14e
Alexander <freemanspeed0738@gmail.com>
2021-03-14 14:18:06 +0200

    user info in profile page

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -3,9 +3,11 @@ import Preloader from '../../common/Preloader/Preloader';
 import classes from './ProfileInfo.module.css';
 
 const ProfileInfo = (props) => {
-    if(!props.profile){
-        return <Preloader/>
+    if (!props.profile) {
+        return <Preloader />
     }
+
+    let UserContacts = Object.entries(props.profile.contacts).map(([key,value]) => <li>{key} : {value}</li>)
 
 
     return (
@@ -15,10 +17,21 @@ const ProfileInfo = (props) => {
             </div>
             <div className={classes.descriptionBlock}>
                 ava + description
+                <div>About Me:
+                    <div>{props.profile.fullName}</div>
+                    <div>{props.profile.aboutMe}</div>
+                </div>
+                <div>Contacts:  
+                    <ul>{UserContacts} </ul>
+                </div>
+                <div>looking for a job: 
+                    <div>{props.profile.lookingForAJob}</div>
+                    <div>{props.profile.lookingForAJobDescription}</div>
+                </div>
             </div>
 
         </div>
-        
+
     );
 }
 
